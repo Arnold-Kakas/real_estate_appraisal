e967c211d71c87477346055c966b0e5d5bf7dc7a
Arnold-Kakas <kakasarnold@gmail.com>
2022-12-21 14:24:55 +0100

    0

--- scrape.R
+++ scrape.R
@@ -2,7 +2,7 @@ library(tidyverse)
 library(rvest)
 
 library(usethis)
-usethis::use_git_config(user.name= "Arnold-Kakas", user.email = "kakasarnold@gmail.com")
+usethis::use_git_config(user.name = "Arnold-Kakas", user.email = "kakasarnold@gmail.com")
 
 install.packages("gitcreds")
 library(gitcreds)
@@ -37,19 +37,24 @@ for (i in 1:number_of_pages) {
     html_nodes("a") %>%
     html_attr("href")
   advertisements <- rbind(advertisements, data.frame(price, type_of_real_estate, address, link, stringsAsFactors = FALSE))
-} 
+}
 
 advertisements_cleaned <- advertisements %>%
   separate(type_of_real_estate, c("type", "area"), sep = " • ") %>%
-  separate(address, c("a", "b", "c"), sep = ", ") %>% 
-  mutate(rooms = case_when(str_detect(type, "byt") ~ substr(type, 1, 1),
-                           str_detect(type, "Garsónka") ~ "1,5",
-                           str_detect(type, "Dvojgarsónka") ~ "2,5"),
-         price = str_replace(price, " €", ""),
-         area = str_replace(area, " m²", ""),
-         type = case_when(!str_detect(type, "byt") ~ type,
-                          str_detect(type, "byt") ~ "Byt")) %>% 
-  unite("address",c(6, 5, 4), sep = ", ", na.rm = TRUE, remove = TRUE) %>% 
-  separate(address, c("district", "municipality", "street"), sep = ", ") %>% 
+  separate(address, c("a", "b", "c"), sep = ", ") %>%
+  mutate(
+    rooms = case_when(
+      str_detect(type, "byt") ~ substr(type, 1, 1),
+      str_detect(type, "Garsónka") ~ "1,5",
+      str_detect(type, "Dvojgarsónka") ~ "2,5"
+    ),
+    price = str_replace(price, " €", ""),
+    area = str_replace(area, " m²", ""),
+    type = case_when(
+      !str_detect(type, "byt") ~ type,
+      str_detect(type, "byt") ~ "Byt"
+    )
+  ) %>%
+  unite("address", c(6, 5, 4), sep = ", ", na.rm = TRUE, remove = TRUE) %>%
+  separate(address, c("district", "municipality", "street"), sep = ", ") %>%
   filter(price != "Cena dohodou", str_detect(district, "okres"))
-
