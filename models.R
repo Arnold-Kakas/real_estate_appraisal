e3571a188dea0a2a9b0c88f94063d6b5fb5bfc1b
Arnold-Kakas <112856554+Arnold-Kakas@users.noreply.github.com>
2023-02-03 14:52:35 +0100

    model changes

--- models.R
+++ models.R
@@ -1,134 +1,137 @@
 # create and evaluate models
 # import libs and data
 library(pacman)
-p_load(rio, tidyverse, tidymodels, GGally, mice, patchwork, vip)
+p_load(rio, tidyverse, tidymodels, GGally, mice, patchwork, vip, dataPreparation)
 
 houses <- import("data/houses.rds")
 apartments <- import("data/apartments.rds")
 
 # clean data
-houses_cleaned <- houses %>% 
-  mutate(#price = log10(price+1),
-         usable_area = as.numeric(usable_area),
-         built_up_area = as.numeric(built_up_area),
-         land_area = as.numeric(land_area)
+houses_cleaned <- houses %>%
+  mutate( # price = log10(price+1),
+    usable_area = as.numeric(usable_area),
+    built_up_area = as.numeric(built_up_area),
+    land_area = as.numeric(land_area)
   )
 
-apartments_cleaned <- apartments %>% 
-  mutate(#price = log10(price),
-         usable_area = as.numeric(usable_area),
-         rooms = as.numeric(rooms)
+apartments_cleaned <- apartments %>%
+  mutate( # price = log10(price),
+    usable_area = as.numeric(usable_area),
+    rooms = as.numeric(rooms)
   )
-
-ggpairs(houses_cleaned[,c(1, 6, 7, 10, 11)])
-ggpairs(apartments_cleaned[,c(1, 6, 8,11, 12)])
 
 # impute missing values
 set.seed(123)
-houses_cleaned <- 
-  houses_cleaned %>% 
-  mice(m = 5,maxit = 5, method = "rf", ntree = 10)
+houses_cleaned <-
+  houses_cleaned %>%
+  mice(m = 5, maxit = 5, method = "rf", ntree = 10)
 houses_cleaned <- complete(houses_cleaned, 1)
 
 set.seed(234)
-apartments_cleaned <- 
-  apartments_cleaned %>% 
-  mice(m = 5,maxit = 5, method = "rf", ntree = 10)
+apartments_cleaned <-
+  apartments_cleaned %>%
+  mice(m = 5, maxit = 5, method = "rf", ntree = 10)
 apartments_cleaned <- complete(apartments_cleaned, 1)
 
-
 # mutate character vector (didn't do it in first cleaning due to slow imputation after this change)
-houses_cleaned <- houses_cleaned %>% 
-  mutate(condition = replace_na(condition, "not provided"),
-         district = as_factor(district),
-         municipality = as_factor(municipality),
-         type = as_factor(type),
-         commission_in_price = as_factor(commission_in_price)
-  )
+houses_cleaned <- houses_cleaned %>%
+  mutate(
+    condition = replace_na(condition, "not provided"),
+    district = as_factor(district),
+    municipality = as_factor(municipality),
+    type = as_factor(type),
+    commission_in_price = as_factor(commission_in_price)
+  ) %>% 
+  remove_sd_outlier(cols = c("price", "usable_area", "built_up_area", "land_area"), n_sigmas = 2) # remove outliers with value +-mean +2sd
 
-apartments_cleaned <- apartments_cleaned %>% 
-  mutate(condition = replace_na(condition, "not provided"),
-         district = as_factor(district),
-         municipality = as_factor(municipality),
-         condition = as_factor(condition),
-         type = as_factor(type),
-         commission_in_price = as_factor(commission_in_price)
-  )
+apartments_cleaned <- apartments_cleaned %>%
+  mutate(
+    condition = replace_na(condition, "not provided"),
+    district = as_factor(district),
+    municipality = as_factor(municipality),
+    condition = as_factor(condition),
+    type = as_factor(type),
+    commission_in_price = as_factor(commission_in_price)
+  ) %>% 
+  remove_sd_outlier(cols = c("price", "usable_area"), n_sigmas = 2) # remove outliers with value +-mean +2sd
+
+ggpairs(houses_cleaned[, c(1, 6, 7, 10, 11)])
+ggpairs(apartments_cleaned[, c(1, 6, 8, 11, 12)])
 
 # EDA
 houses_BoxCox <- recipe(houses_train, price ~ .) %>%
-  step_rm(lon, lat, lon_rotated, lat_rotated) %>% 
-  step_BoxCox(all_numeric_predictors()) %>% 
-  prep() %>% 
+  step_rm(lon, lat, lon_rotated, lat_rotated) %>%
+  step_BoxCox(all_numeric_predictors()) %>%
+  prep() %>%
   bake(new_data = NULL)
 
 houses_log <- recipe(houses_train, price ~ .) %>%
-  step_rm(lon, lat, lon_rotated, lat_rotated) %>% 
-  step_log(all_numeric_predictors()) %>% 
-  prep() %>% 
+  step_rm(lon, lat, lon_rotated, lat_rotated) %>%
+  step_log(all_numeric_predictors()) %>%
+  prep() %>%
   bake(new_data = NULL)
 
 houses_norm <- recipe(houses_train, price ~ .) %>%
-  step_rm(lon, lat, lon_rotated, lat_rotated) %>% 
-  step_normalize(all_numeric_predictors()) %>% 
-  prep() %>% 
+  step_rm(lon, lat, lon_rotated, lat_rotated) %>%
+  step_normalize(all_numeric_predictors()) %>%
+  prep() %>%
   bake(new_data = NULL)
 
-# Plot distribution of price before and after transformations
-p1 <- ggplot(data = houses_cleaned, aes(x = price)) + 
-  geom_density() + 
+# Plot distribution of built_up_area before and after transformations
+p1 <- ggplot(data = houses_cleaned, aes(x = built_up_area)) +
+  geom_density() +
   ggtitle("price before transformations")
 
-p2 <- ggplot(data = houses_BoxCox, aes(x = price)) + 
-  geom_density() + 
+p2 <- ggplot(data = houses_BoxCox, aes(x = built_up_area)) +
+  geom_density() +
   ggtitle("price after Box-Cox transform")
 
-p3 <- ggplot(data = houses_norm, aes(x = price)) + 
-  geom_density() + 
+p3 <- ggplot(data = houses_norm, aes(x = built_up_area)) +
+  geom_density() +
   ggtitle("price after normalization")
 
-p4 <- ggplot(data = houses_log, aes(x = price)) + 
-  geom_density() + 
+p4 <- ggplot(data = houses_log, aes(x = built_up_area)) +
+  geom_density() +
   ggtitle("price after log transform")
 
-(p1 + p2)/ (p3 + p4)
+(p1 + p2) / (p3 + p4)
 
 apartments_BoxCox <- recipe(apartments_train, price ~ .) %>%
-  step_rm(lon, lat, lon_rotated, lat_rotated) %>% 
-  step_BoxCox(all_numeric_predictors()) %>% 
-  prep() %>% 
+  step_rm(lon, lat, lon_rotated, lat_rotated) %>%
+  step_BoxCox(all_numeric_predictors()) %>%
+  prep() %>%
   bake(new_data = NULL)
 
 apartments_log <- recipe(apartments_train, price ~ .) %>%
-  step_rm(lon, lat, lon_rotated, lat_rotated) %>% 
-  step_log(all_numeric_predictors()) %>% 
-  prep() %>% 
+  step_rm(lon, lat, lon_rotated, lat_rotated) %>%
+  step_log(all_numeric_predictors()) %>%
+  prep() %>%
   bake(new_data = NULL)
 
 apartments_norm <- recipe(apartments_train, price ~ .) %>%
-  step_rm(lon, lat, lon_rotated, lat_rotated) %>% 
-  step_normalize(all_numeric_predictors()) %>% 
-  prep() %>% 
+  step_rm(lon, lat, lon_rotated, lat_rotated) %>%
+  step_normalize(all_numeric_predictors()) %>%
+  prep() %>%
   bake(new_data = NULL)
 
-# Plot distribution of price before and after transformations
-p5 <- ggplot(data = apartments_cleaned, aes(x = price)) + 
-  geom_density() + 
+# Plot distribution of usable_area before and after transformations
+p5 <- ggplot(data = apartments_cleaned, aes(x = usable_area)) +
+  geom_density() +
   ggtitle("price before transformations")
 
-p6 <- ggplot(data = apartments_BoxCox, aes(x = price)) + 
-  geom_density() + 
+p6 <- ggplot(data = apartments_BoxCox, aes(x = usable_area)) +
+  geom_density() +
   ggtitle("price after Box-Cox transform")
 
-p7 <- ggplot(data = apartments_norm, aes(x = price)) + 
-  geom_density() + 
+p7 <- ggplot(data = apartments_norm, aes(x = usable_area)) +
+  geom_density() +
   ggtitle("price after normalization")
 
-p8 <- ggplot(data = apartments_log, aes(x = price)) + 
-  geom_density() + 
+p8 <- ggplot(data = apartments_log, aes(x = usable_area)) +
+  geom_density() +
   ggtitle("price after log transform")
 
-(p5 + p6)/ (p7 + p8)
+(p5 + p6) / (p7 + p8)
 
 # split dataframes to train(80)/test(20)
 set.seed(345)
@@ -158,45 +161,49 @@ apartments_train_boots <- bootstraps(apartments_train, times = 25)
 # recipes
 houses_xgboost_recipe <- recipe(houses_train, price ~ .) %>%
   step_rm(lat, lon, municipality) %>%
-  step_log(usable_area, built_up_area, land_area) %>%
-#  step_normalize(usable_area, built_up_area, land_area) %>%
+  step_BoxCox(usable_area, built_up_area, land_area) %>%
+  #  step_normalize(usable_area, built_up_area, land_area) %>%
   step_other(all_nominal(), threshold = 0.01) %>%
-  step_dummy(all_nominal()) 
+  step_dummy(all_nominal())
 
 apartments_xgboost_recipe <- recipe(apartments_train, price ~ .) %>%
   step_rm(lat, lon, municipality) %>%
-  step_impute_knn(all_nominal_predictors()) %>% 
-  step_log(usable_area) %>%
-  step_normalize(usable_area) %>%
+  step_impute_knn(all_nominal_predictors()) %>%
+  step_BoxCox(usable_area) %>%
+#  step_normalize(usable_area) %>%
   step_other(all_nominal(), threshold = 0.01) %>%
   step_dummy(all_nominal())
 
 # models
-xgb_model <- 
+xgb_model <-
   boost_tree(
     trees = 1000, loss_reduction = tune(),
     tree_depth = tune(), min_n = tune(),
     mtry = tune(), sample_size = tune(),
     learn_rate = tune()
-  ) %>% 
-  set_mode("regression") %>% 
+  ) %>%
+  set_mode("regression") %>%
   set_engine("xgboost", nthread = 6) # use 6 cores for multithreading
 
 # workflows
-houses_workflow <- workflow() %>% add_recipe(houses_xgboost_recipe) %>% add_model(xgb_model)
-apartments_workflow <- workflow() %>% add_recipe(apartments_xgboost_recipe) %>% add_model(xgb_model)
+houses_workflow <- workflow() %>%
+  add_recipe(houses_xgboost_recipe) %>%
+  add_model(xgb_model)
+apartments_workflow <- workflow() %>%
+  add_recipe(apartments_xgboost_recipe) %>%
+  add_model(xgb_model)
 
 # parameters
 houses_xgboost_params <- parameters(
   trees(),
-  tree_depth(), min_n(), 
-  finalize(mtry(), houses_train)  
+  tree_depth(), min_n(),
+  finalize(mtry(), houses_train)
 )
 
 apartments_xgboost_params <- parameters(
   trees(), learn_rate(),
-  tree_depth(), min_n(), 
-  finalize(mtry(), apartments_train)  
+  tree_depth(), min_n(),
+  finalize(mtry(), apartments_train)
 )
 
 # optimization
@@ -243,12 +250,12 @@ apartments_best_model <- select_best(apartments_xgboost_res, "rmse")
 
 # finalize models
 houses_final_model <- finalize_model(xgb_model, houses_best_model)
-houses_workflow    <- houses_workflow %>% update_model(houses_final_model)
-houses_xgb_fit     <- fit(houses_workflow, data = houses_train)
+houses_workflow <- houses_workflow %>% update_model(houses_final_model)
+houses_xgb_fit <- fit(houses_workflow, data = houses_train)
 
 apartments_final_model <- finalize_model(xgb_model, apartments_best_model)
-apartments_workflow    <- apartments_workflow %>% update_model(apartments_final_model)
-apartments_xgb_fit     <- fit(apartments_workflow, data = apartments_train)
+apartments_workflow <- apartments_workflow %>% update_model(apartments_final_model)
+apartments_xgb_fit <- fit(apartments_workflow, data = apartments_train)
 
 houses_xgb_fit %>%
   fit(data = houses_train) %>%
@@ -261,15 +268,15 @@ apartments_xgb_fit %>%
   vip(geom = "point")
 
 # evaluate
-houses_pred <- 
+houses_pred <-
   predict(houses_xgb_fit, houses_test) %>%
   bind_cols(houses_test)
 
- houses_plot1 <- 
-   houses_pred %>% 
-   ggplot(aes(x = .pred, y = price))+
-   geom_point() + 
-   geom_abline(intercept = 0, col = "red")
+houses_plot1 <-
+  houses_pred %>%
+  ggplot(aes(x = .pred, y = price)) +
+  geom_point() +
+  geom_abline(intercept = 0, col = "red")
 
 
 houses_plot2 <-
@@ -288,7 +295,7 @@ apartments_pred <-
 
 apartments_plot1 <-
   apartments_pred %>%
-  ggplot(aes(x = .pred, y = price))+
+  ggplot(aes(x = .pred, y = price)) +
   geom_point() +
   geom_abline(intercept = 0, col = "red")
 
@@ -301,7 +308,7 @@ apartments_plot2 <-
   geom_density(alpha = .2) +
   labs(x = "", y = "")
 
-apartments_plot1/apartments_plot2
+apartments_plot1 / apartments_plot2
 
 # to do
 # check for julia silge video on many factor levels
