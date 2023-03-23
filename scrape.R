7a578834848d875e5105363a9b5e78938633dfb7
Arnold-Kakas <112856554+Arnold-Kakas@users.noreply.github.com>
2023-03-23 19:34:29 +0100

    rselenium update

--- scrape.R
+++ scrape.R
@@ -23,6 +23,7 @@ plan(multisession, workers = 6)
 
 advertisements <- future_map_dfr(1:number_of_pages, function(i) {
   page_content <- read_html(paste0(site, i))
+  Sys.sleep(3) # wait 3 seconds
   price <- page_content %>%
     html_nodes(xpath = '//*[@class="advertisement-item--content__price col-auto pl-0 pl-md-3 pr-0 text-right mt-2 mt-md-0 align-self-end"]') %>%
     html_attr("data-adv-price")
@@ -52,13 +53,16 @@ remDr <- rs_driver_object$client
 # open a browser
 remDr$open()
 remDr$navigate('https://www.nehnutelnosti.sk/')
-Sys.sleep(5) #wait for 5 seconds
+Sys.sleep(3) #wait for 5 seconds
 # deal with cookies
 remDr$switchToFrame(remDr$findElement(using = 'xpath', '//*[@id="sp_message_iframe_710573"]'))
 remDr$findElement(using = 'xpath', '//*[@id="notice"]/div[5]/div[2]/button')$clickElement()
 
 # wait for another pop up window
-Sys.sleep(10)
+Sys.sleep(20)
+
+height <- as.numeric(remDr$executeScript('return document.documentElement.scrollHeight'))/2
+remDr$executeScript(paste('window.scrollTo(0, ',height,');')) # scroll to middle of the page
 
 # decline option 
 remDr$findElement(using = 'xpath', '//*[@id="onesignal-slidedown-cancel-button"]')$clickElement()
@@ -70,59 +74,77 @@ additional_info_df <- map_dfr(advertisements$link, function(i) {
 
   height <- as.numeric(remDr$executeScript('return document.documentElement.scrollHeight')) - 2400 # 2400 pixels = lenght from botton which does not change
   remDr$executeScript(paste('window.scrollTo(0, ',height,');')) # scroll to living index
-
-  page <- remDr$findElement(using = 'xpath', '//*[@id="map-filter-container"]')
-  page_html <- page$getElementAttribute('outerHTML')
-  page_html <- read_html(page_html[[1]])
   
-  info_text <- page_html %>% 
-    html_nodes(xpath = '//*[contains(concat( " ", @class, " " ), concat( " ", "text-inner", " " ))]') %>%
-    html_text2() %>%
-    as.character() %>%
-    str_trim() %>% 
-    str_squish()
+  Sys.sleep(3)
   
-  info_details <- page_html %>% 
-    html_nodes(xpath = '//*[contains(concat( " ", @class, " " ), concat( " ", "col-md-4", " " ))]//div') %>%
-    html_text2()
-  
-  info <- as.data.frame(info_details) %>%
-    separate(info_details, sep = ": ", c("info", "status")) %>%
-    filter(info %in% info_names) %>%
-    pivot_wider(names_from = "info", values_from = "status")
-
-  temp_col_names <- colnames(info)
-  
-  condition <- ifelse("Stav" %in% temp_col_names, info$Stav, NA)
-  usable_area <- ifelse("Úžit. plocha" %in% temp_col_names, info$"Úžit. plocha", NA)
-  built_up_area <- ifelse("Zast. plocha" %in% temp_col_names, info$"Zast. plocha", NA)
-  land_area <- ifelse("Plocha pozemku" %in% temp_col_names, info$"Plocha pozemku", NA)
-  commission_in_price <- ifelse("Provízia zahrnutá v cene" %in% temp_col_names, info$"Provízia zahrnutá v cene", NA)
-  
-
   tryCatch({
-    index <- page_html %>% 
-      html_nodes(xpath = '//*[@id="totalCityperformerWrapper"]/div/p[1]/span') %>%
-      html_text2() # %>% 
-    # str_extract("[:digit:]+") %>% 
-    # as.numeric()
+    suppressMessages(page <- remDr$findElement(using = 'xpath', '//*[@id="map-filter-container"]'))
   }, error = function(e) {
-    index <- NA
+    page <- NA_character_
   })  
   
-  tryCatch({
-    additional_chars <- page_html %>% 
-      html_nodes(xpath = '//*[@id="additional-features-modal-button"]/ul') %>% 
-      html_text() %>% 
-      str_squish()
-  }, error = function(e) {
-    additional_chars <- NA
-  })
-  
-  
-
-  tibble(info_text = info_text, additional_characteristics = additional_chars, index_of_living = index, condition = condition, usable_area = usable_area, built_up_area = built_up_area, land_area = land_area, commission_in_price = commission_in_price)
-  })
+  if (is.na(page)) {
+    info_text <- NA_character_
+    condition <- NA_character_
+    usable_area <- NA_character_
+    built_up_area <- NA_character_
+    land_area <- NA_character_
+    commission_in_price <- NA_character_
+    index <- NA_character_
+    additional_chars <- NA_character_
+    
+    tibble(info_text = info_text, additional_characteristics = additional_chars, index_of_living = index, condition = condition, usable_area = usable_area, built_up_area = built_up_area, land_area = land_area, commission_in_price = commission_in_price)
+    
+  } else {
+      page_html <- page$getElementAttribute('outerHTML')
+      page_html <- read_html(page_html[[1]])
+      
+      info_text <- page_html %>% 
+        html_nodes(xpath = '//*[contains(concat( " ", @class, " " ), concat( " ", "text-inner", " " ))]') %>%
+        html_text2() %>%
+        as.character() %>%
+        str_trim() %>% 
+        str_squish()
+      
+      info_details <- page_html %>% 
+        html_nodes(xpath = '//*[contains(concat( " ", @class, " " ), concat( " ", "col-md-4", " " ))]//div') %>%
+        html_text2()
+      
+      info <- as.data.frame(info_details) %>%
+        separate(info_details, sep = ": ", c("info", "status")) %>%
+        filter(info %in% info_names) %>%
+        pivot_wider(names_from = "info", values_from = "status")
+    
+      temp_col_names <- colnames(info)
+      
+      condition <- ifelse("Stav" %in% temp_col_names, info$Stav, NA)
+      usable_area <- ifelse("Úžit. plocha" %in% temp_col_names, info$"Úžit. plocha", NA)
+      built_up_area <- ifelse("Zast. plocha" %in% temp_col_names, info$"Zast. plocha", NA)
+      land_area <- ifelse("Plocha pozemku" %in% temp_col_names, info$"Plocha pozemku", NA)
+      commission_in_price <- ifelse("Provízia zahrnutá v cene" %in% temp_col_names, info$"Provízia zahrnutá v cene", NA)
+      
+    
+      tryCatch({
+        index <- page_html %>% 
+          html_nodes(xpath = '//*[@id="totalCityperformerWrapper"]/div/p[1]/span') %>%
+          html_text2() # %>% 
+        # str_extract("[:digit:]+") %>% 
+        # as.numeric()
+      }, error = function(e) {
+        index <- NA
+      })  
+      
+      tryCatch({
+        additional_chars <- page_html %>% 
+          html_nodes(xpath = '//*[@id="additional-features-modal-button"]/ul') %>% 
+          html_text() %>% 
+          str_squish()
+      }, error = function(e) {
+        additional_chars <- NA
+      })
+      tibble(info_text = info_text, additional_characteristics = additional_chars, index_of_living = index, condition = condition, usable_area = usable_area, built_up_area = built_up_area, land_area = land_area, commission_in_price = commission_in_price)
+      }
+      })
 
 rs_driver_object$client$close()
 rs_driver_object$server$stop()
