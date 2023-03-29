c44e6adba5839e07b860353fad1921afa5e766a1
Arnold-Kakas <112856554+Arnold-Kakas@users.noreply.github.com>
2023-03-29 19:28:46 +0200

    .

--- scrape.R
+++ scrape.R
@@ -40,50 +40,51 @@ advertisements <- future_map_dfr(1:number_of_pages, function(i) {
   tibble(price = price, type_of_real_estate = type_of_real_estate, address = address, link = link)
 })
 
+# Define a wrapper function that handles the errors
+safe_find_element <- possibly(function(page, xpath) {
+  page$findElement(using = "xpath", xpath)
+}, NA_character_)
+
+
 # Additional info from web
 
 # start the server
-rs_driver_object <- rsDriver(browser = 'chrome',
-                             chromever = '111.0.5563.41',
-                             verbose = FALSE,
-                             port = free_port(random = TRUE))
-                          
+rs_driver_object <- rsDriver(
+  browser = "chrome",
+  chromever = "111.0.5563.41",
+  verbose = FALSE,
+  port = free_port(random = TRUE)
+)
+
 # create a client object
 remDr <- rs_driver_object$client
 # open a browser
 remDr$open()
-remDr$navigate('https://www.nehnutelnosti.sk/')
-Sys.sleep(3) #wait for 5 seconds
+remDr$navigate("https://www.nehnutelnosti.sk/")
+Sys.sleep(3) # wait for 5 seconds
 # deal with cookies
-remDr$switchToFrame(remDr$findElement(using = 'xpath', '//*[@id="sp_message_iframe_710573"]'))
-remDr$findElement(using = 'xpath', '//*[@id="notice"]/div[5]/div[2]/button')$clickElement()
+remDr$switchToFrame(remDr$findElement(using = "xpath", '//*[@id="sp_message_iframe_710573"]'))
+remDr$findElement(using = "xpath", '//*[@id="notice"]/div[5]/div[2]/button')$clickElement()
 
 # wait for another pop up window
 Sys.sleep(20)
 
-height <- as.numeric(remDr$executeScript('return document.documentElement.scrollHeight'))/2
-remDr$executeScript(paste('window.scrollTo(0, ',height,');')) # scroll to middle of the page
+height <- as.numeric(remDr$executeScript("return document.documentElement.scrollHeight")) / 2
+remDr$executeScript(paste("window.scrollTo(0, ", height, ");")) # scroll to middle of the page
 
-# decline option 
-remDr$findElement(using = 'xpath', '//*[@id="onesignal-slidedown-cancel-button"]')$clickElement()
+# decline option
+remDr$findElement(using = "xpath", '//*[@id="onesignal-slidedown-cancel-button"]')$clickElement()
 
+advertisements[8510, ] %>% select(link)
 
 additional_info_df <- map_dfr(advertisements$link, function(i) {
-  remDr$navigate(i)
-  Sys.sleep(5) #wait for 5 seconds
+  page_check <- possibly(
+    remDr$navigate(i) %>%
+      Sys.sleep(5),
+    NA_character_
+  )
 
-  height <- as.numeric(remDr$executeScript('return document.documentElement.scrollHeight')) - 2400 # 2400 pixels = lenght from botton which does not change
-  remDr$executeScript(paste('window.scrollTo(0, ',height,');')) # scroll to living index
-  
-  Sys.sleep(3)
-  
-  tryCatch({
-    suppressMessages(page <- remDr$findElement(using = 'xpath', '//*[@id="map-filter-container"]'))
-  }, error = function(e) {
-    page <- NA_character_
-  })  
-  
-  if (is.na(page)) {
+  if (is.na(page_check)) {
     info_text <- NA_character_
     condition <- NA_character_
     usable_area <- NA_character_
@@ -92,59 +93,93 @@ additional_info_df <- map_dfr(advertisements$link, function(i) {
     commission_in_price <- NA_character_
     index <- NA_character_
     additional_chars <- NA_character_
-    
+
     tibble(info_text = info_text, additional_characteristics = additional_chars, index_of_living = index, condition = condition, usable_area = usable_area, built_up_area = built_up_area, land_area = land_area, commission_in_price = commission_in_price)
-    
   } else {
-      page_html <- page$getElementAttribute('outerHTML')
+    remDr$navigate(i)
+    Sys.sleep(5) # wait for 5 seconds
+
+    height <- as.numeric(remDr$executeScript("return document.documentElement.scrollHeight")) - 2400 # 2400 pixels = lenght from botton which does not change
+    remDr$executeScript(paste("window.scrollTo(0, ", height, ");")) # scroll to living index
+
+    Sys.sleep(3)
+
+    # tryCatch({
+    #   suppressMessages(page <- remDr$findElement(using = 'xpath', '//*[@id="map-filter-container"]'))
+    # }, error = function(e) {
+    #   page <- NA_character_
+    # })
+
+    page <- safe_find_element(remDr, '//*[@id="map-filter-container"]')
+
+    if (is.na(page)) {
+      info_text <- NA_character_
+      condition <- NA_character_
+      usable_area <- NA_character_
+      built_up_area <- NA_character_
+      land_area <- NA_character_
+      commission_in_price <- NA_character_
+      index <- NA_character_
+      additional_chars <- NA_character_
+
+      tibble(info_text = info_text, additional_characteristics = additional_chars, index_of_living = index, condition = condition, usable_area = usable_area, built_up_area = built_up_area, land_area = land_area, commission_in_price = commission_in_price)
+    } else {
+      page_html <- page$getElementAttribute("outerHTML")
       page_html <- read_html(page_html[[1]])
-      
-      info_text <- page_html %>% 
+
+      info_text <- page_html %>%
         html_nodes(xpath = '//*[contains(concat( " ", @class, " " ), concat( " ", "text-inner", " " ))]') %>%
         html_text2() %>%
         as.character() %>%
-        str_trim() %>% 
+        str_trim() %>%
         str_squish()
-      
-      info_details <- page_html %>% 
+
+      info_details <- page_html %>%
         html_nodes(xpath = '//*[contains(concat( " ", @class, " " ), concat( " ", "col-md-4", " " ))]//div') %>%
         html_text2()
-      
+
       info <- as.data.frame(info_details) %>%
         separate(info_details, sep = ": ", c("info", "status")) %>%
         filter(info %in% info_names) %>%
         pivot_wider(names_from = "info", values_from = "status")
-    
+
       temp_col_names <- colnames(info)
-      
+
       condition <- ifelse("Stav" %in% temp_col_names, info$Stav, NA)
       usable_area <- ifelse("Úžit. plocha" %in% temp_col_names, info$"Úžit. plocha", NA)
       built_up_area <- ifelse("Zast. plocha" %in% temp_col_names, info$"Zast. plocha", NA)
       land_area <- ifelse("Plocha pozemku" %in% temp_col_names, info$"Plocha pozemku", NA)
       commission_in_price <- ifelse("Provízia zahrnutá v cene" %in% temp_col_names, info$"Provízia zahrnutá v cene", NA)
-      
-    
-      tryCatch({
-        index <- page_html %>% 
-          html_nodes(xpath = '//*[@id="totalCityperformerWrapper"]/div/p[1]/span') %>%
-          html_text2() # %>% 
-        # str_extract("[:digit:]+") %>% 
-        # as.numeric()
-      }, error = function(e) {
-        index <- NA
-      })  
-      
-      tryCatch({
-        additional_chars <- page_html %>% 
-          html_nodes(xpath = '//*[@id="additional-features-modal-button"]/ul') %>% 
-          html_text() %>% 
-          str_squish()
-      }, error = function(e) {
-        additional_chars <- NA
-      })
+
+
+      tryCatch(
+        {
+          index <- page_html %>%
+            html_nodes(xpath = '//*[@id="totalCityperformerWrapper"]/div/p[1]/span') %>%
+            html_text2() # %>%
+          # str_extract("[:digit:]+") %>%
+          # as.numeric()
+        },
+        error = function(e) {
+          index <- NA
+        }
+      )
+
+      tryCatch(
+        {
+          additional_chars <- page_html %>%
+            html_nodes(xpath = '//*[@id="additional-features-modal-button"]/ul') %>%
+            html_text() %>%
+            str_squish()
+        },
+        error = function(e) {
+          additional_chars <- NA
+        }
+      )
       tibble(info_text = info_text, additional_characteristics = additional_chars, index_of_living = index, condition = condition, usable_area = usable_area, built_up_area = built_up_area, land_area = land_area, commission_in_price = commission_in_price)
-      }
-      })
+    }
+  }
+})
 
 rs_driver_object$client$close()
 rs_driver_object$server$stop()
@@ -178,21 +213,24 @@ advertisements_cleaned <- advertisements %>%
   unite("address0", c(6, 5, 4), sep = ", ", na.rm = TRUE, remove = TRUE) %>% # reordering to keep all districts in first column
   separate(address0, c("district", "municipality", "street"), sep = ", ") %>%
   filter(str_detect(district, "okres")) %>%
-  mutate(price = as.numeric(str_replace_all(price, " ",""))) %>% 
+  mutate(price = as.numeric(str_replace_all(price, " ", ""))) %>%
   select(-area)
 
 
 # save the old file to histo folder for further use in predictive analyses
 
 if (file.exists("data/advertisements.rds")) {
-   histo_rds <- import("data/advertisements.rds") %>% 
-  mutate(timestamp = as.Date(file.info("data/advertisements.rds")$ctime))
-  histo_date <- file.info("data/advertisements.rds")$ctime %>% as.Date() %>% as.character() %>% str_replace_all("-", "_")
+  histo_rds <- import("data/advertisements.rds") %>%
+    mutate(timestamp = as.Date(file.info("data/advertisements.rds")$ctime))
+  histo_date <- file.info("data/advertisements.rds")$ctime %>%
+    as.Date() %>%
+    as.character() %>%
+    str_replace_all("-", "_")
   saveRDS(histo_rds, paste0("data/histo/advertisements", histo_date, ".rds"))
 }
 
 # save scraped data
-# write.csv2(text_long, "data/texts.csv") 
+# write.csv2(text_long, "data/texts.csv")
 saveRDS(text_long, file = "data/texts.rds") # instead of csv due to size reduction
 
 # write.csv2(advertisements_cleaned, "data/advertisements.csv")
@@ -201,12 +239,12 @@ saveRDS(advertisements_cleaned, file = "data/advertisements.rds") # instead of c
 
 
 
-# 
+#
 # # Start a Docker container running the Selenium Chrome browser image
 # system("docker run -d -p 4445:4444 selenium/standalone-chrome")
 # # Check that the container is running
 # system("docker ps")
-# 
+#
 # remDr <- RSelenium::remoteDriver(remoteServerAddr = "localhost",
 #                                  port = 4445L,
 #                                  browserName = "chrome")
@@ -218,16 +256,10 @@ saveRDS(advertisements_cleaned, file = "data/advertisements.rds") # instead of c
 # pagesource <- remDr$findElement(using = 'xpath', '//*[contains(concat( " ", @class, " " ), concat( " ", "align-self-stretch", " " ))]')
 # page_html <- pagesource$getPageSource()[[1]] %>% xml2::read_html()
 # page_html %>% html_nodes(xpath = '//*[@id="totalCityperformerWrapper"]/div/p[1]/span') %>% html_text2()
-# 
-# 
+#
+#
 # # Stop Docker container
 # system(paste0("docker stop ", container_id))
-# 
+#
 # # Remove Docker container
 # system(paste0("docker rm ", container_id))
-
-
-
-
-
-
